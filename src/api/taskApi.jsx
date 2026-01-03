361f75fcc4e24010e7a74206c6811637d4142a1c
Subhiksha Jain <mj8194328@gmail.com>
2026-01-03 14:24:48 +0530

    Fix Netlify deploy config

--- src/api/taskApi.jsx
+++ src/api/taskApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://backend-2-tx51.onrender.com/api"
 });
 
 export const getTasks = () => API.get("/tasks");
